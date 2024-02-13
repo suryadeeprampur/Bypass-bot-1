f6e8b03dc6cf87e8d63c3d264c2807180e03da84
Amm0ni4 <gi0a0pc2@anonaddy.me>
2024-02-13 22:06:21 +0100

    simplify the shareus.io script

--- extra_bypasses/shareus.io.user.js
+++ extra_bypasses/shareus.io.user.js
@@ -8,25 +8,17 @@
 (function() {
     'use strict';
     if (window.location.hostname === 'shareus.io') {
-        document.addEventListener('DOMContentLoaded', function() {
-            // Define the button selector
-            const buttonSelector = "#root > div > main > div.main-container-1 > div.main-container-2 > div:nth-child(1) > div.adunit-container > button";
-        
-            // Function to click the button
+        document.addEventListener('DOMContentLoaded', function() { // Wait for the page to be loaded
+
             function clickButton() {
-                // Find the button element
-                const button = document.querySelector(buttonSelector);
+                const button = document.querySelector("#root > div > main > div.main-container-1 > div.main-container-2 > div:nth-child(1) > div.adunit-container > button");
                 if (button) {
                     button.click();
-                    console.log("Button clicked!");
                     //clearInterval(intervalId); // Stop attempting once clicked
-                } else {
-                    console.log("Button not found!");
                 }
             }
-        
-            // Set interval to attempt clicking every 1 second
-            const intervalId = setInterval(clickButton, 1000);
+            const intervalId = setInterval(clickButton, 1000); // Set interval to attempt clicking every 1 second
+
         });
     }
 })();
