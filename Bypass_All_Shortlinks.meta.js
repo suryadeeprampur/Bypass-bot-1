039f56d074c9c62b6372d76beb939fb1056b5749
Amm0ni4 <gi0a0pc2@anonaddy.me>
2024-02-16 16:51:38 +0100

    minor fix for stfly
    https://greasyfork.org/en/scripts/431691-bypass-all-shortlinks/discussions/229063

--- Bypass_All_Shortlinks.meta.js
+++ Bypass_All_Shortlinks.meta.js
@@ -321,7 +321,7 @@
 // @match *://*.letsboost.net/*
 // @match *://*.web1s.asia/*
 // @include /^(https?:\/\/)(.+)?((hookeaudio|pennbookcenter|publicananker|fun88bk).com|fun88.info.vn)(\/.*)/
-// @include /^(https?:\/\/)(.+)?(stfly.cc|stfly.xyz|techtrendmakers.com|(blogbux|blogesque).net)(\/.*)/
+// @include /^(https?:\/\/)(.+)?(stfly.cc|stfly.xyz|techtrendmakers.com|(exploreera|blogbux|blogesque).net)(\/.*)/
 // @match *://*.coins-town.com/*
 // @include /^(https?:\/\/)(.+)?(slinkware.com|aghtas.com|mazen-ve3.com|youssefsayed.com|alghtas.xyz|newworldnew.com|tackaway.com)(\/.*)/
 // @include /^(https?:\/\/)(.+)?(link.goto.com.np|(appkamods|bankvacency|mealcold|otomi-games).com)(\/.*)/
