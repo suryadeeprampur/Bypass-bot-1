83e506b86c14de4a1faeb9cf544fdb5c09bf893f
Amm0ni4 <gi0a0pc2@anonaddy.me>
2024-02-27 00:34:00 +0100

    bumped subversion

--- Bypass_All_Shortlinks.user.js
+++ Bypass_All_Shortlinks.user.js
@@ -3,7 +3,7 @@
 // @namespace  Violentmonkey Scripts
 // @run-at     document-start
 // @author     Amm0ni4
-// @version        91.4.1
+// @version        91.4.2
 // @grant          GM_setValue
 // @grant          GM_getValue
 // @grant          GM_addStyle
