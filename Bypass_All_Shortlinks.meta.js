ae40febb225f04e69ebc0ad9d087198b8920fe83
Amm0ni4 <gi0a0pc2@anonaddy.me>
2024-03-17 01:32:43 +0100

    bump subversion

--- Bypass_All_Shortlinks.meta.js
+++ Bypass_All_Shortlinks.meta.js
@@ -3,7 +3,7 @@
 // @namespace  Violentmonkey Scripts
 // @run-at     document-start
 // @author     Amm0ni4
-// @version        91.5.13
+// @version        91.5.14
 // @grant          GM_setValue
 // @grant          GM_getValue
 // @grant          GM_addStyle
