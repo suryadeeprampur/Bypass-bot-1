bc172776e54c7030b9d956d7dc079471d717ca4e
Amm0ni4 <gi0a0pc2@anonaddy.me>
2024-07-27 12:44:24 +0200

    fix for link detection of bypass.city

--- extra_bypasses/bypass.city.user.js
+++ extra_bypasses/bypass.city.user.js
@@ -12,10 +12,15 @@
     const isValidUrl = url => { try { new URL(url); return true; } catch (error) { return false; } };
 
     function makeUrlClickable(element) {
-        let url = element.textContent.match(/(https?:\/\/\S+)/)[0];
+        let initialUrl = element.textContent;
+        let url = element.textContent.replace('The resolved url is: ', '')
+        if (!url.startsWith('http')) {
+            url = 'https://' + url;
+        }
+        url = url.match(/(https?:\/\/\S+)/)[0];
         if (url && isValidUrl(url) && !url.endsWith('...')) {
             let link = document.createElement('a'); link.href = url; link.textContent = url;
-            element.innerHTML = element.innerHTML.replace(url, link.outerHTML);
+            element.innerHTML = 'The resolved url is: ' + element.innerHTML.replace(initialUrl, link.outerHTML);
             return url;
         }
     }
@@ -24,10 +29,12 @@
       window.addEventListener('load', function() { //After window loaded
         let checkInterval = setInterval(function() { //Check every 0.5s if the link is available
             let linkContainerElement = document.querySelector('div.mantine-fn96xv:nth-child(3)');
-            if (linkContainerElement && linkContainerElement.innerHTML.includes('http')) {
+            if (linkContainerElement) {
                 clearInterval(checkInterval);
                 let targetUrl = makeUrlClickable(linkContainerElement);
-                isValidUrl(targetUrl) && window.location.assign(targetUrl);
+                if (isValidUrl(targetUrl)) {
+                    setTimeout(function() { window.location.assign(targetUrl); }, 4000);
+                }
             }
         }, 500);
       });
