ccdd20b84094275f8abf25c9708a8584b0ce03cb
Amm0ni4 <gi0a0pc2@anonaddy.me>
2024-10-05 21:08:22 +0200

    Revert "minor fix for rinku"
    This reverts commit 8d687732981035acbde77bff0eaf7dcf7c56a467.

--- Bypass_All_Shortlinks.user.js
+++ Bypass_All_Shortlinks.user.js
@@ -2373,11 +2373,6 @@
         redirectIfNotDisabled('a.get-link');
     }) : null;
 
-    // for rinku.me
-    if (/(actualpost|americanstylo|beautifulfashionnailart|dadinthemaking|glowandglamcorner|listofthis|lobirtech).com/.test(url)) {
-        preventForcedFocusOnWindow();
-    }
-
 })();
 
 (function() {
--- extra_bypasses/extra_bypasses.user.js
+++ extra_bypasses/extra_bypasses.user.js
@@ -881,11 +881,6 @@
         redirectIfNotDisabled('a.get-link');
     }) : null;
 
-    // for rinku.me
-    if (/(actualpost|americanstylo|beautifulfashionnailart|dadinthemaking|glowandglamcorner|listofthis|lobirtech).com/.test(url)) {
-        preventForcedFocusOnWindow();
-    }
-
 })();
 
 (function() {
