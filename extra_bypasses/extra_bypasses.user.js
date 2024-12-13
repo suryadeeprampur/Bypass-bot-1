1c5fbb8ee13cd700ccd7e53d697c63d6a4622e98
Amm0ni4 <gi0a0pc2@anonaddy.me>
2024-12-13 01:58:56 +0100

    disable timerskip for profitsfly now it breaks the site
    https://codeberg.org/Amm0ni4/bypass-all-shortlinks-debloated/issues/191

--- extra_bypasses/extra_bypasses.user.js
+++ extra_bypasses/extra_bypasses.user.js
@@ -1148,17 +1148,17 @@
     /^https:\/\/modijiurl\.com\/[^\/]+\/\?mid=.*$/.test(url) ? redirectIfNotDisabled('#getLinkButton') : null;
 
     // profitsfly partly skip timer
-    /^https:\/\/.*\.(tradeshowrating.com|historyofyesterday.com|playonpc.online|quins.us)\/.*/.test(url) ? (() => {
-        afterDOMLoaded(function() {
-            function setTimer() {
-                if (window.wT9882 > 5) {
-                    window.wT9882 = 1;
-                }
-            }
-            window.wT9882 = 5;
-            setInterval(setTimer, 1000);
-        });
-    })() : null;
+//    /^https:\/\/.*\.(tradeshowrating.com|historyofyesterday.com|playonpc.online|quins.us)\/.*/.test(url) ? (() => {
+//        afterDOMLoaded(function() {
+//            function setTimer() {
+//                if (window.wT9882 > 5) {
+//                    window.wT9882 = 1;
+//                }
+//            }
+//            window.wT9882 = 5;
+//            setInterval(setTimer, 1000);
+//        });
+//    })() : null;
 
     // sittingonclouds.com .net
     /paster.gg/.test(url) ? redirect(decodeURIComponent(url.split('&link=')[1].split('&')[0])) : null;
