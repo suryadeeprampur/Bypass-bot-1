3700d7fa24562cca28f618e61104179266ab5966
Amm0ni4 <gi0a0pc2@anonaddy.me>
2025-01-09 20:00:51 +0100

    add profitsfly domain
    https://codeberg.org/Amm0ni4/bypass-all-shortlinks-debloated/issues/191

--- extra_bypasses/profitsfly_reload_helper.user.js
+++ extra_bypasses/profitsfly_reload_helper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        profitsfly reload helper
-// @include     /^https:\/\/.*\.(tradeshowrating.com|historyofyesterday.com|playonpc.online|quins.us)\/.*/
+// @include     /^https:\/\/.*\.((tradeshowrating|historyofyesterday|insurelean).com|playonpc.online|quins.us)\/.*/
 // @run-at      document-start
 // ==/UserScript==
 
@@ -8,7 +8,7 @@
 (function() {
     "use strict";
 
-    const domainRegex = /^https:\/\/.*\.(tradeshowrating.com|historyofyesterday.com|playonpc.online|quins.us)\/.*/;
+    const domainRegex = /^https:\/\/.*\.((tradeshowrating|historyofyesterday|insurelean).com|playonpc.online|quins.us)\/.*/;
     if (domainRegex.test(window.location.href)) {
 
         // ---RELOAD DEAD-END PAGES---
